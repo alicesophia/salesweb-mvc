63fbde375507110e40ea6c99f9239508158b424f
alicesophia <alicesophiakoch@gmail.com>
2022-02-24 17:03:49 -0300

    Implementing grouping search

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -34,8 +34,20 @@ namespace SalesWebMVC.Controllers {
             return View(result);
         }
 
-        public IActionResult GroupingSearch() {
-            return View();
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate) {
+            if (!minDate.HasValue) {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue) {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
 
     }
