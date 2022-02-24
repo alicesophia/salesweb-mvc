0bc082548a80179d3c0642d7e4ba47e1487e1936
alicesophia <alicesophiakoch@gmail.com>
2022-02-24 00:54:36 -0300

    Seller details and eager loading

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -57,5 +57,18 @@ namespace SalesWebMVC.Controllers {
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services {
     public class SellerService {
@@ -23,7 +24,8 @@ namespace SalesWebMVC.Services {
         }
 
         public Seller FindById(int id) {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id) {
